eb83882b73c930e4ddcf6cd63c98c65d4c1b4bc7
Julen Ferrero <julenferre@gmail.com>
2016-10-24 21:43:35 +0200

    io.c -> ausazko hitza ezin du hartu

--- iturburu-kodea/io.c
+++ iturburu-kodea/io.c
@@ -1,5 +1,9 @@
-#include <stdio.h>
+#include <stdio.h>//fopen
+#include <string.h> //strcpy
 #include <math.h> //round-erako
+#include <stdlib.h> //rand
+
+#define MAX_BUF 1024
 
 /*
  * @brief Gai bat emanda, gai horretako hitz bat bueltatzen dituen metodoa
@@ -7,34 +11,75 @@
  * @return Zerrendako hitz bat bueltatuko du
  */
 
-
-char[] hitzaHartu(char gaia[]) {
+char * hitzaHartu(char gaia[]) {
     
     /*Fitxategia irakurtzen da*/
-    int i; 
-    FILE *fitxategia;
-    const char *hitzak[100];
-    char pathosoa[];
-    strcpy(pathosoa,"./zerrendak/");
+    int i, ausazkoa; 
+    FILE * fitxategia = NULL;
+    char *hitzak[MAX_BUF];
+    char lerroa[MAX_BUF];
+    char pathosoa[MAX_BUF];
+    size_t len = 0;
+    ssize_t read;
+    
+    strcpy(pathosoa,"/home/julen/Dropbox/EHU3/SZA/PO2/zerrendak/");
     strcat(pathosoa,gaia);
-    fitxategia= fopen(pathosoa, "r");
+    strtok(pathosoa,"\n");
+        
+    fitxategia=fopen(pathosoa, "r");
     
     if (!fitxategia)  {
         printf("%s gaia ez da existitzen\n", gaia);
-        return 1;
+        return "1";
     }
     
     i = 0;
     
-    while (!feof(fitxategia))
-    {  
-        fgets(hitzak[i], sizeof(hitzak[i]), fitxategia);
+    while (fgets(lerroa, MAX_BUF, fitxategia)!=NULL) {
         i ++;
+        hitzak[i] = lerroa;
+        printf("%d. hitza: %s", i, hitzak[i]);
     }
+    printf("\n");
     
     fclose (fitxategia);
     
-    ausazkoa = round(rand() * i);
-     
-    return hitzak[ausazkoa];#include <math.h>
+    ausazkoa = rand() % i;
+    
+    printf("Ausazkoa: %d. hitza: %s\n", ausazkoa, (char *) hitzak[ausazkoa]);
+    
+    return hitzak[ausazkoa];
+}
+
+int main(int argc, char *argv[]){
+    
+    int caseZenb;
+    char buf[MAX_BUF];
+    
+    // Egiaztatu argumentu bat pasa dela.    
+    if(argc != 2)
+    {
+            fprintf(stderr, "Erabilera: %s <ekintza>\n", argv[0]);
+            exit(1);
+    }
+    
+    //Egiaztatu komandoa existitzen dela
+    if(strcmp(argv[0],"hitzaHartu")){
+        caseZenb = 1;
+    }
+    else { caseZenb = 0;}
+	
+    switch (caseZenb){
+        case 1:
+            printf("Sartu gaia: ");
+            while(fgets(buf, MAX_BUF, stdin) != NULL)
+            {            
+                printf("Zure hitza: %s\n",hitzaHartu(buf));                    
+            }
+            break;
+        default:
+            printf("Ekintza hori ez da existitzen\n");
+            break;
+    }
+    return 1;
 }
